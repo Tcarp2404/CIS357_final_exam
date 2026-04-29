b4d537d646540913ecbc16bdbadba40464c4ba13
Tcarp2404 <160423235+Tcarp2404@users.noreply.github.com>
2026-04-28 20:14:37 -0400

    New Android Studio project

---
+++ app/src/main/java/edu/gvsu/cis/cis357_carpenter_final_exam/MainActivity.kt
@@ -0,0 +1,47 @@
+package edu.gvsu.cis.cis357_carpenter_final_exam
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.gvsu.cis.cis357_carpenter_final_exam.ui.theme.Cis357_carpenter_final_examTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Cis357_carpenter_final_examTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Cis357_carpenter_final_examTheme {
+        Greeting("Android")
+    }
+}
